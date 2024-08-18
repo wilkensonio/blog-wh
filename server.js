55b2ab7284ad17ae91f058b2d03fc7d9f9341b25
Wilkenson <wilkensoncode@gmail.com>
2024-08-18 19:34:55 -0400

    edit resume and nav

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
     res.send('Wrong URL');
-});
+}); 
+
  
 app.listen(8000, () => {
     console.log('Server is running on port 8000'); 
