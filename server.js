1c7f113b7437eb6a14672dd0671c60100ddd2985
Wilkenson <wilkensoncode@gmail.com>
2024-08-24 14:51:23 -0400

    remove cache from all routes and cache only needed route

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ const resumeRoutes = require('./api/routes/resume.route');
 const methodOverride = require('method-override'); 
 const path = require('path');
 const cookieParser = require('cookie-parser'); 
-const compression = require('compression');
-const apicache = require('apicache');
+const compression = require('compression'); 
 
 const dotenv = require('dotenv'); 
 
 dotenv.config();
 
-const app = express(); 
-
-const cache = apicache.middleware
-app.use(cache('120 minutes'));
+const app = express();  
 
 app.set('views', [
     path.join(__dirname, './client/views'),
@@ -51,13 +47,11 @@ app.use(cookieParser());
 mongoose.connect(process.env.MONGO)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => { console.log(err)
-});
- 
+}); 
 
 app.use(express.static(path.join(__dirname, './api/public'))); 
 
 app.use(methodOverride('_method')); 
-
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -74,8 +68,6 @@ app.use('/admin', adminRoutes);
 
 // resume
 app.use('/about', resumeRoutes);
-
-
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
