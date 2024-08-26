e540e92b3f5a8b559056909cea81828b6d52c803
Wilkenson <wilkensoncode@gmail.com>
2024-08-25 23:31:21 -0400

    Add syle to mermaid and code graph and edit form_fields

--- api/models/article.js
+++ api/models/article.js
@@ -65,8 +65,9 @@ const postProcessHtml = (html) => {
     // Replaced `language-mermaid` with `mermaid`
     processedHtml = processedHtml.replace(/class="language-mermaid"/g, 'class="mermaid"');
     processedHtml = processedHtml.replace(/<table>/g, '<table class="table table-borderless text-white">');
+    
 
-    processedHtml = processedHtml.replace(/<pre><code class="language-(?!mermaid)(\S*)">/g, '<pre><code class="language-$1">');
+    processedHtml = processedHtml.replace(/<pre><code class="language-(?!mermaid)(\S*)">/g, '<pre><code class="language-$1 bg-programming-code">');
    
     return processedHtml;
 };
