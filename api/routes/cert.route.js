3525be42ad76d0b4670a0463f191098d3b7142c6
Wilkenson <wilkensoncode@gmail.com>
2025-03-08 19:05:27 -0500

    added cert

---
+++ api/routes/cert.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { isAdminOrWriter, isAuthenticated } = require('../middelware/auth.js');
+const upload = require('../utils/multerConfig.js');
+const {certs, formCert, deleteCert, createCert, editCertForm, updateCert} = require('../controllers/cert.contoller.js');
+
+const router = express.Router();
+
+router.get('/', certs);
+router.get('/new', isAuthenticated, isAdminOrWriter, formCert);
+
+router.post('/new', isAuthenticated, isAdminOrWriter,  upload.single('image'), createCert);
+router.delete('/delete/:id', deleteCert); 
+ 
+
+router.get('/edit/:id', isAuthenticated, isAdminOrWriter, editCertForm);
+router.put('/edit/:id', isAuthenticated, isAdminOrWriter, upload.single('image'), updateCert);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./api/routes/home.route');
 const adminRoutes = require('./api/routes/admin.route');
 const resumeRoutes = require('./api/routes/resume.route'); 
 const projectsRoutes = require('./api/routes/projects.route'); 
+const certsRoutes = require('./api/routes/cert.route'); 
 const methodOverride = require('method-override'); 
 const path = require('path');
 const cookieParser = require('cookie-parser'); 
@@ -20,10 +21,14 @@ const app = express();
 
 // Path to the uploads directory
 const uploadDir = path.join(__dirname, './api/public', 'projectUploads');
+const uploadCert = path.join(__dirname, './api/public', 'certsUploads');
 
 // Create the uploads directory if it doesn't exist
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
+}
+if (!fs.existsSync(uploadCert)) {
+    fs.mkdirSync(uploadCert);
 }
 
 
@@ -58,7 +63,7 @@ app.use(compression(
 app.use(cookieParser());
 mongoose.connect(process.env.MONGO)
 .then(() => console.log('Connected to MongoDB'))
-.catch(err => { console.log(err)
+.catch(err => { console.error(err)
 }); 
 
 app.use(express.static(path.join(__dirname, './api/public')));  
@@ -67,19 +72,15 @@ app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', homeRoutes);
+app.use('/', homeRoutes); 
+ 
 // resume
-app.get('/about', (req, res) => {
-    const isAdmin = req.user && req.user.isAdmin === true;
-    const isWriter = req.user && req.user.isWriter === true; 
-    res.render('resume/index', { isAdmin: isAdmin, isWriter: isWriter, resume: true });
-});
-
-app.get('/certs', (req, res) => {
-    res.render('certs/index');
+app.get('/about', (req, res) => { 
+    res.render('resume/index', auth(req));
 });
 
 app.use('/projects', projectsRoutes);
+app.use('/certs', certsRoutes);
 
 app.use('/posts', articleRoutes); 
 app.use('/admin', adminRoutes);
@@ -97,3 +98,10 @@ app.get('*', (req, res) => {
 app.listen(8000, () => {
     console.log('Server is running on port 8000'); 
 });
+
+// pass when page is loaded to check if user is admin or writer
+const auth = (req) => {
+    const isAdmin = req.user && req.user.isAdmin === true;
+    const isWriter = req.user && req.user.isWriter === true; 
+    return { isAdmin: isAdmin, isWriter: isWriter, resume: true }
+}
